88ce3dd6c7522277ac0016cb62d07d7b59f2aed8
luckrafael <lrs.rafael@hotmail.com>
2022-02-16 13:46:59 -0300

    Adding users list

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'josil@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2021/05/31/05/55/naruto-6297820_960_720.jpg',
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'riamasil@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2021/05/16/07/26/naruto-6257398_960_720.png',
+
+    },
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email: 'juliafral@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2021/07/03/10/54/naruto-6383640_960_720.jpg ',
+    },
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email: 'rafamontee@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2021/02/04/12/02/character-5981113_960_720.png',
+    },
+];
